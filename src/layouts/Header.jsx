7d0052b57710fddb3366dfbafa4b8c59ea4bbd77
SophieVergari <sophie.vergari.pro@gmail.com>
2025-03-04 16:16:31 +0100

    maj navbar

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 function Header() {
     return (
         <>
-            <header className='border border-b-black w-full shadow-sm bg-[#6262620D]'>
-                <nav className='h-24 flex items-center justify-between mx-35 '>
+            <header className='fixed top-0 left-0 z-50 w-full bg-blur-nav'>
+                <nav className='h-18 flex items-center justify-between mx-35 '>
                     <Link className='relative w-fit ' to='/'>
                         <h1 className='t-briller-logo relative z-10'>blindify</h1>
                         <span className='t-briller-logo-vide absolute top-1 left-1 z-0'>blindify</span>
