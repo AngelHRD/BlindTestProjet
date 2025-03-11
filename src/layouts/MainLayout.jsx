74c555b65d0bfa37c24ae2b5bf10ca492b841961
Angel H <hryndaangel@gmail.com>
2025-03-11 09:35:19 +0100

    Suppression de la récupération de l'URL actuelle dans MainLayout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,8 +2,6 @@ import { Outlet } from 'react-router-dom';
 import Header from './Header';
 
 function MainLayout() {
-    const location = useLocation(); // Récupère l'URL actuelle
-
     // Masquer le Header si l'utilisateur est sur la page d'erreur
     const hideHeader = location.pathname === '/404';
 
