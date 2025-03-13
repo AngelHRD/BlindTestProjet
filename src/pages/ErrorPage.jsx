d905ddcb8e2b6b70cd074e0df6c9f31ebad9537e
Angel H <hryndaangel@gmail.com>
2025-03-13 13:17:38 +0100

    responsive errorPage

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -4,14 +4,14 @@ const ErrorPage = () => {
     return (
         <section className='container h-screen mx-auto flex flex-col items-center justify-center'>
             <div className='relative text-center'>
-                <h2 className='text-4xl t-owners z-10'>Erreur 404</h2>
-                <h1 className='text-4xl t-owners z-10 mt-5'> Page non trouvée</h1>
+                <h2 className='text-2xl lg:text-5xl t-owners z-10'>Erreur 404</h2>
+                <h1 className='text-2xl lg:text-5xl t-owners z-10 mt-5'> Page non trouvée</h1>
             </div>
 
             <div className='mt-10'>
                 <Link
                     to='/'
-                    className='!text-black t-briller !text-xl px-6 py-3 bg-[#7ff000] rounded-2xl'
+                    className='!text-black t-briller text-sm px-3 py-1.5 lg:text-2xl lg:px-6 lg:py-3 bg-[#7ff000] rounded-2xl'
                     aria-label="Retour à l'accueil"
                 >
                     Retour à l&apos;accueil
