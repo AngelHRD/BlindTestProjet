5f21b5af2f5695029c655aeabc91bcedf1ada1ba
SophieVergari <sophie.vergari.pro@gmail.com>
2025-03-20 13:14:42 +0100

    update css scorepage

--- src/pages/ScorePage.jsx
+++ src/pages/ScorePage.jsx
@@ -41,39 +41,20 @@ function ScorePage() {
     }
 
     return (
-        <section className='blur-color-green-score h-screen lg:h-[calc(100vh-72px)] flex flex-col justify-center'>
-            <div className='container mx-auto p-6 lg:py-12 flex-grow flex flex-col justify-between lg:items-center gap-y-5 lg:gap-y-10 h-full'>
-                {/* ShadowBox  */}
-                <div className='w-full max-w-2xl lg:max-w-7xl  flex flex-col text-center bg-blur p-6 py-12 text-white h-full flex-grow'>
-                    {/* Titre */}
-                    <div className='flex-grow-[1] flex flex-col justify-start items-center gap-y-4'>
-                        <h2 className='t-owners main-title'>
-                            Blin<span className='t-briller main-title'>d </span> test
-                        </h2>
-                        <h3 className='t-briller-vide second-title -mt-5'>{genre}</h3>
-                    </div>
+        <section className=' h-screen lg:h-[calc(100vh-72px)] flex flex-col justify-center'>
+            <div className='container mx-auto p-6 lg:py-12 flex flex-col justify-center lg:items-center gap-y-5 lg:gap-y-10 h-full'>
+                <p className='text-2xl md:text-4xl para'>Bien joué ! Tu as trouvé</p>
+                <div className='h-[500px] w-[500px] lg:text-xl linear scale-[1.03] animate-rotate-border rounded-full bg-conic/[from_var(--border-angle)] from-gray-800 from-70% via-[chartreuse] via-90% to-gray-800 to-100% p-px shadow-[0_0_100px_45px_rgba(127,240,0,0.2)] transition-all duration-500 ease-in-out'>
+                    <div className=' flex flex-col h-full w-full items-center justify-center rounded-full bg-[#141313] text-center text-xl text-[chartreuse] transition-colors duration-500 ease-in-out'>
+                        {/* Texte centré  */}
 
-                    {/* Texte centré  */}
-                    <div className='flex-grow-[5] flex flex-col justify-center items-center w-full gap-y-4'>
-                        <p className='text-2xl md:text-4xl para'>Bien joué ! Tu as trouvé</p>
-                        <p className='t-owners text-[10rem] lg:text-[13rem] leading-none !text-black !font-medium'>
-                            {score}
-                        </p>
-                        <p className='text-2xl lg:text-3xl para'>Titres sur {maxSongs}</p>
-                    </div>
-
-                    {/* Texte + bouton*/}
-                    <div className='flex-grow-[1] flex flex-col justify-end items-center gap-y-4'>
-                        <p className='text-lg lg:text-2xl para mx-2'>Améliore toi avec nos autres blind test !</p>
-
-                        <ButtonPerso
-                            to='/genres'
-                            text='Plus de blind test !'
-                            width='lg:w-1/2 w-full'
-                            height='lg:h-14 h-10'
-                        />
+                        <div>
+                            <p className='font-score-countdown leading-none'> {score}</p>{' '}
+                            <p className='text-2xl lg:text-3xl para leading-0'>titres sur {maxSongs} </p>
+                        </div>
                     </div>
                 </div>
+                <ButtonPerso to='/genres' text='Plus de blind test !' width='lg:w-1/4 w-full' height='lg:h-14 h-10' />
             </div>
         </section>
     );
