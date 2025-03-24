8e6d01b1b493320a90e40884bc271bcb752b5f87
Angel H <hryndaangel@gmail.com>
2025-03-24 20:59:29 +0100

    Modif type button

--- src/components/ButtonPerso.jsx
+++ src/components/ButtonPerso.jsx
@@ -2,29 +2,38 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { forwardRef } from 'react';
 
-const ButtonPerso = forwardRef(({ to, text, width, height, onClick, hidden = 'block', disabled }, ref) => {
-    const classes = ` ${width} ${height} ${hidden}  lg:text-xl linear scale-[1.03] animate-rotate-border cursor-pointer rounded-lg bg-conic/[from_var(--border-angle)] from-gray-800 from-70% via-[chartreuse] via-90% to-gray-800 to-100% p-px shadow-lg transition-all duration-500 ease-in-out hover:shadow-[chartreuse]/10 hover:scale-105 ${
-        disabled ? 'opacity-50 pointer-events-none' : ''
-    }`;
+const ButtonPerso = forwardRef(
+    ({ to, text, width, height, onClick, hidden = 'block', disabled, type = 'button' }, ref) => {
+        const classes = ` ${width} ${height} ${hidden}  lg:text-xl linear scale-[1.03] animate-rotate-border cursor-pointer rounded-lg bg-conic/[from_var(--border-angle)] from-gray-800 from-70% via-[chartreuse] via-90% to-gray-800 to-100% p-px shadow-lg transition-all duration-500 ease-in-out hover:shadow-[chartreuse]/10 hover:scale-105 ${
+            disabled ? 'opacity-50 pointer-events-none' : ''
+        }`;
 
-    if (onClick) {
+        if (onClick || type === 'submit') {
+            return (
+                <button
+                    ref={ref}
+                    onClick={onClick}
+                    type={type}
+                    className={classes}
+                    aria-label={text}
+                    disabled={disabled}
+                >
+                    <p className='flex h-full w-full items-center justify-center rounded-lg bg-neutral-900 hover:bg-neutral-800 text-center text-xl text-[chartreuse] transition-colors duration-500 ease-in-out'>
+                        {text}
+                    </p>
+                </button>
+            );
+        }
+
         return (
-            <button ref={ref} onClick={onClick} className={classes} aria-label={text} disabled={disabled}>
+            <Link to={disabled ? '#' : to} className={classes} aria-label={text} ref={ref}>
                 <p className='flex h-full w-full items-center justify-center rounded-lg bg-neutral-900 hover:bg-neutral-800 text-center text-xl text-[chartreuse] transition-colors duration-500 ease-in-out'>
                     {text}
                 </p>
-            </button>
+            </Link>
         );
-    }
-
-    return (
-        <Link to={disabled ? '#' : to} className={classes} aria-label={text} ref={ref}>
-            <p className='flex h-full w-full items-center justify-center rounded-lg bg-neutral-900 hover:bg-neutral-800 text-center text-xl text-[chartreuse] transition-colors duration-500 ease-in-out'>
-                {text}
-            </p>
-        </Link>
-    );
-});
+    },
+);
 
 ButtonPerso.displayName = 'ButtonPerso';
 
@@ -36,6 +45,7 @@ ButtonPerso.propTypes = {
     onClick: PropTypes.func,
     hidden: PropTypes.string,
     disabled: PropTypes.bool,
+    type: PropTypes.string,
 };
 
 export default ButtonPerso;
