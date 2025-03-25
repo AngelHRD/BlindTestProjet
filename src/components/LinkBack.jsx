b2df4d5760cdda7013067bdc8bb5af3f6343db38
Angel H <hryndaangel@gmail.com>
2025-03-25 10:16:48 +0100

    Ajout de la possibilité de naviguer avec un lien ou de revenir en arrière avec un bouton dans le composant LinkBack

--- src/components/LinkBack.jsx
+++ src/components/LinkBack.jsx
@@ -1,12 +1,10 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-function LinkBack({ text = 'Retour' }) {
-    return (
-        <button
-            onClick={() => window.history.back()}
-            className='md:flex flex items-center gap-1 lg:my-5 my-2.5 cursor-pointer w-fit hover:opacity-80'
-            aria-label={text}
-        >
+function LinkBack({ text = 'Retour', to }) {
+    const commonClasses = 'md:flex flex items-center gap-1 lg:my-5 my-2.5 cursor-pointer w-fit hover:opacity-80';
+    const IconAndText = () => (
+        <>
             <svg
                 xmlns='http://www.w3.org/2000/svg'
                 fill='none'
@@ -19,12 +17,23 @@ function LinkBack({ text = 'Retour' }) {
                 <path strokeLinecap='round' strokeLinejoin='round' d='M15.75 19.5 8.25 12l7.5-7.5' />
             </svg>
             <p className='text-white text-lg md:text-xl'>{text}</p>
+        </>
+    );
+
+    return to ? (
+        <Link to={to} className={commonClasses} aria-label={text}>
+            <IconAndText />
+        </Link>
+    ) : (
+        <button onClick={() => window.history.back()} className={commonClasses} aria-label={text}>
+            <IconAndText />
         </button>
     );
 }
 
 LinkBack.propTypes = {
     text: PropTypes.string,
+    to: PropTypes.string,
 };
 
 export default LinkBack;
